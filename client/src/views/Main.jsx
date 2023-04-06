295263420727a50b3c39c99aa4ad4d2cfdc67ba7
Peter Disney <peter.w.disney@gmail.com>
2023-04-05 20:24:41 -0700

    added jumbotron

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -4,15 +4,22 @@ import "../index.css";
 
 const Main = () => {
   return (
-    <div >
-    <Navbar />
-      <div className="fadeInUp">
-        <div>
-          <div>
-          <h1>Peter Disney</h1>
-          </div>
-          <div>
-            <h2>Full-Stack Software Developer</h2>
+    <div>
+      <Navbar />
+      <div class="bg-gray-100 py-24">
+        <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="bg-white shadow-xl rounded-lg overflow-hidden">
+            <div class="px-4 py-5 sm:px-6">
+              <h2 class="text-6xl font-bold text-gray-800">
+                Peter Disney
+              </h2>
+              <p class="mt-1 text-2xl text-gray-600">
+                Full-Stack Software Developer
+              </p>
+            </div>
+            <div class="px-4 py-5 sm:p-6">
+
+            </div>
           </div>
         </div>
       </div>
