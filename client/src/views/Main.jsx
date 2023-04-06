c2fe77ff45725e38494efb6dd288728a05b9f492
Peter Disney <peter.w.disney@gmail.com>
2023-04-05 20:14:45 -0700

    scroll feature

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "../components/GenericComponents/Navbar";
+import "../index.css";
 
 const Main = () => {
   return (
-    <div>
-      <Navbar />
+    <div >
+    <Navbar />
+      <div className="fadeInUp">
+        <div>
+          <div>
+          <h1>Peter Disney</h1>
+          </div>
+          <div>
+            <h2>Full-Stack Software Developer</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
