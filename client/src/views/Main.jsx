8879c54b5040eead2171881a25b423e28a489766
Peter Disney <peter.w.disney@gmail.com>
2023-04-05 20:24:59 -0700

    adding section tag

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -5,24 +5,25 @@ import "../index.css";
 const Main = () => {
   return (
     <div>
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
+      <section>
+
       <div class="bg-gray-100 py-24">
         <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="bg-white shadow-xl rounded-lg overflow-hidden">
             <div class="px-4 py-5 sm:px-6">
-              <h2 class="text-6xl font-bold text-gray-800">
-                Peter Disney
-              </h2>
+              <h2 class="text-6xl font-bold text-gray-800">Peter Disney</h2>
               <p class="mt-1 text-2xl text-gray-600">
                 Full-Stack Software Developer
               </p>
             </div>
-            <div class="px-4 py-5 sm:p-6">
-
-            </div>
+            <div class="px-4 py-5 sm:p-6"></div>
           </div>
         </div>
       </div>
+      </section>
     </div>
   );
 };
