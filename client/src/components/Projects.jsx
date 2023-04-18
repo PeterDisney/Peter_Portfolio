fd94e0af2fca78b197dd962bce33726429e1f801
Peter Disney <peter.w.disney@gmail.com>
2023-04-18 14:42:20 -0700

    added projects, updated info, added coming soon and skew

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -11,22 +11,22 @@ const Project = ({
   skills,
 }) => {
   return (
-    <div className="shadow-lg p-6 rounded-xl">
+    <div className="shadow-lg bg-white p-6 rounded-xl mb-8">
       <p className="text-2xl font-bold mb-4">{name}</p>
       <p className="text-lg mb-2">{projectDescription}</p>
-      <img className="shadow-xl rounded-lg" src={appImage} alt={name} />
+      <img className="shadow-xl rounded-lg mx-auto" src={appImage} alt={name} />
       <p className=" text-lg rounded-lg text-center mt-4 p-1">{skills}</p>
       <div className="flex space-x-4 mt-4 flex items-center justify-center">
-        <a className="flex-grow" href={appLink} target="_blank">
+        <div className="flex-grow" href={appLink} target="_blank">
           <a
             className="text-black hover:text-l transition-all duration-200 "
             target="_blank"
-            href="https://github.com/PeterDisney"
+            href={appLink} 
           >
             <FontAwesomeIcon size="3x" icon={faChrome} />
           </a>
-        </a>
-        <a className="flex-grow" href={appRepo} target="_blank">
+        </div>
+        <div className="flex-grow" href={appRepo} target="_blank">
           <a
             className="text-black hover:text-l transition-all duration-200 "
             target="_blank"
@@ -34,7 +34,7 @@ const Project = ({
           >
             <FontAwesomeIcon size="3x" icon={faGithub} />
           </a>
-        </a>
+        </div>
       </div>
     </div>
   );
