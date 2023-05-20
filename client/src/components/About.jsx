d1829718926131747f7b1e6a0a4ba3f164b4170b
Peter Disney <peter.w.disney@gmail.com>
2023-05-19 20:22:11 -0700

    xl p4 sm break point main div

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="shadow-lg bg-white p-6 sm:px-6 lg:px-8 rounded-xl">
+    <div className="shadow-xl bg-white p-6 sm:px-4 lg:px-8 rounded-xl">
       <div className="flex justify-center text-4xl font-bold mb-4">
         <p>About</p>
       </div>
